3999a2d0562d5fd860f3b87e46ce5691bfa1bf53
Russell Branca <chewbranca@apache.org>
2026-02-10 10:17:55 -0800

    Too much

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "pet-count" {
   description = "How many pets do you want? A dog"
   type        = number
-  default     = 5
+  default     = 4
 }
 
 variable "word-length" {
   description = "length of word. oh yeah"
   type        = number
-  default     = 12
+  default     = 11
 }
